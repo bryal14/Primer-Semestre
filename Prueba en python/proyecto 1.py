cc809d4d864fa6098cead6ed74cbb629431c5976
bryal14 <150269328+bryal14@users.noreply.github.com>
2024-01-16 17:41:08 -0500

    .

---
+++ Prueba en python/proyecto 1.py
@@ -0,0 +1,37 @@
+"""creacion de un codigo que nos permita identificar mediante """
+##estructuras de control el mayor de dos numeros ingresados por teclados 
+
+"""def obtener_mayor(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2 
+
+def main():
+ 
+        num1 = int(input("Ingresa el primer numero: "))
+        num2 = int(input("Ingresa el segundo numero: "))
+        mayor = obtener_mayor(num1, num2)
+        print(f"El numero mayor es: {mayor}")
+        
+if __name__ == "__main__":
+    main()"""
+    
+
+def main():
+        n1= int
+        n2 = int
+
+n1= (input("Ingresa el primer numero: "))
+n2 =(input("Ingresa el segundo numero: "))
+
+if n1 > n2: 
+    print(f"El numero mayor es: {n1}")
+    print(f"El numero menor es: {n2}")
+    
+    
+else: 
+    print(f"El numero mayor es: {n2}")
+    print(f"El numero menor es: {n1}")
+    
+    main()
